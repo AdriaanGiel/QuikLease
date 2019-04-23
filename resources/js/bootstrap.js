d46f8aed9c2a26c07ab20f5aac39256b551bf65c
Adriaan Giel <a3aan_G@live.nl>
2019-04-23 13:43:14 +0200

    - added uiv library - added public folder to .gitignore

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import * as uiv from 'uiv'
 
 window.Vue = Vue;
 window.VueRouter = VueRouter;
 window.axios = require('axios');
 
 Vue.use(VueRouter);
+Vue.use(uiv);
 
