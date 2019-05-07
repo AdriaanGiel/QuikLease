e171e80f51097557b376c6a9abbec9d904596ee9
Luc Uppenkamp <0944982@hr.nl>
2019-05-07 15:03:18 +0200

    helft van start animatie

--- resources/js/pages/login.vue
+++ resources/js/pages/login.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="background">
         <div class="container">
-            <div class="circle">
+            <div class="logincircle">
             </div>    
             <div class="logo">
                 <IconBase x="0px" y="0px" width="100%" height="100%" viewBox="0 0 803 903" icon="Logo"><Logo class="logosvg"></Logo></IconBase>
@@ -28,7 +28,7 @@
         width: 100vw;
         background-color: black;
         animation-name: colorchangebw;
-        animation-delay: 2s;
+        animation-delay: 1.5s;
         animation-duration: 2s;
         animation-iteration-count: 1;
         animation-fill-mode: forwards;
@@ -41,15 +41,21 @@
         margin: auto auto;
     }
 
-    .circle {
-        position: absolute;
-        left: 25%;
-        top: 30%;
-        height: 500px;
-        width: 500px;
-        border-radius: 50%;
-        margin: auto auto;
+    .logincircle {
+        height: 45vh;
+        width: 45vh;
+        border-radius: 100%;
         background-color: white;
+        position: absolute;
+        top: calc(50% - 22.5vh);
+        left: calc(50% - 22.5vh);
+        -moz-transform: translateX(-50%) translateY(-50%);
+        -webkit-transform: translateX(-50%) translateY(-50%);
+        transform: translateX(-50%) translateY(-50%);
+        animation-name: introcircle;
+        animation-duration: 3s;
+        animation-iteration-count: 1;
+        animation-fill-mode: forwards;
     }
 
     .logo {
@@ -59,6 +65,29 @@
         height: 33%;
         width: 33%;
         margin: auto auto;
+    }
+
+    @keyframes introcircle {
+        0% {
+            transform: scale(0.1);
+            display: block;
+        }
+
+        30% {
+            transform: scale(1);
+        }
+
+        60% {
+            transform: scale(1);
+        }
+        
+        99% {
+            transform: scale(5);
+        }
+        100% {
+            transform: scale(5);
+            display: none;
+        }
     }
 
     @keyframes colorchangebw {
