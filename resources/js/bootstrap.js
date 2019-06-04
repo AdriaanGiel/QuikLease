e12240e86dcdfa046feb00345841587889ee8a29
Adriaan Giel <a3aan_G@live.nl>
2019-06-04 16:25:44 +0200

    up to date with temp login included

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import * as uiv from 'uiv'
-import Field from 'material-components-vue/dist/text-field';
 
 
 window.Vue = Vue;
@@ -11,4 +10,4 @@ window.axios = require('axios');
 Vue.use(VueRouter);
 Vue.use(uiv);
 
-Vue.use(Field);
+// Vue.use(Field);
